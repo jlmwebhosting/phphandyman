910a63689a9ad67e37c3e18af8baf8225f9f04b7
Christian Hotz-Behofsits <chris.hotz.behofists@gmail.com>
2012-08-18 13:07:18 +0200

    Array lib added

---
+++ Array.php
@@ -0,0 +1,29 @@
+<?php
+
+ /**
+  * PHP String library
+  *
+  * @author Christian Hotz-Behofsits <chris@stagdesign.at>
+  * 
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
+
+class Array{
+	
+	public static function in_array($haystack, $needle) { 
+
+	    $i = new RecursiveIteratorIterator(new RecursiveArrayIterator($haystack)); 
+
+	    foreach($i as $item) { 
+	        if($item == $needle) { 
+	            return true; 
+	        } 
+	    } 
+
+	    return false; 
+	}
+
+}
+
+?>
