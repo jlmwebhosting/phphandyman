f8064d35167a50da3737cb1a827158808fe01a4c
Christian Hotz-Behofsits <chris.hotz.behofists@gmail.com>
2012-08-19 11:24:35 +0200

    tail function added

--- File.php
+++ File.php
@@ -15,6 +15,32 @@ class File{
 		return pathinfo($path, PATHINFO_EXTENSION);
 	}
 
+    public static function tail($path, $lines = 10) {
+        $data = '';
+        $block = 4096;
+        $fp = fopen($path, 'r');
+        $max = filesize($path);
+     
+        for($len = 0; $len < $max; $len += $block) {
+            $seek = ($max - $len > $block) ? $block : $max - $len;
+            fseek($fp, ($len + $seek) * -1, SEEK_END);
+            $data = fread($fp, $seek) . $data;
+
+            if(substr_count($data, PHP_EOL) >= $lines + 1) {
+                if(substr($data, strlen($data)-1, 1) !== PHP_EOL) {
+                    $data .= PHP_EOL;
+                }
+
+                preg_match('/(.*?\n){'. $lines .'}$/', $data, $match);
+                fclose($fp);
+                return $match[0];
+            }
+        }
+        
+        fclose($fp);
+        return $data; 
+    }
+
 
 }
 
